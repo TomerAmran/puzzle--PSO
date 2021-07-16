c309e96ad826cb1aa2d56ecbd6914587a5765ce5
tomera <tomera@wix.com>
2021-07-16 14:16:10 +0300

    fdaf

--- test_algorithem.py
+++ test_algorithem.py
@@ -4,13 +4,21 @@ from particle import Particle
 from constants import TILE_SIZE
 from puzzle import Puzzle
 import numpy as np
-
+import itertools
+def all_perms(elements):
+    if len(elements) <=1:
+        yield elements
+    else:
+        for perm in all_perms(elements[1:]):
+            for i in range(len(elements)):
+                # nb elements[0:1] works in both string and list contexts
+                yield perm[:i] + elements[0:1] + perm[i:]
 
 def exhaustive_search():
     puzzle = Puzzle('imgs/small.jpeg', TILE_SIZE)
     puzzle.load()
     ps = []
-    for _ in range(10):
+    for _ in range(200):
         t_copy = copy.deepcopy(puzzle.tiles)
         random.shuffle(t_copy)
         ps.append(Particle(t_copy))
